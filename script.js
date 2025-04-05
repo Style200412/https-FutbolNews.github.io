6da8c260d7824b8edc2db131fc54c4afddf80d0d
Style200412 <intizardinler@gmail.com>
2025-04-05 22:41:55 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+// Şu an için boş veya temel DOMContentLoaded olayı
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Haber sitesi yüklendi!');
+    // Gelecekte buraya dinamik işlevler eklenebilir
+});
